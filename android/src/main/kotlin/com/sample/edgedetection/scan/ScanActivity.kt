57f71508fb6a06ddd9300fb6087682ef4b641735
thuan.le <dthuanle.20@gmail.com>
2023-02-28 18:05:08 +0700

    change ExifInterface to androidx

--- android/src/main/kotlin/com/sample/edgedetection/scan/ScanActivity.kt
+++ android/src/main/kotlin/com/sample/edgedetection/scan/ScanActivity.kt
@@ -3,7 +3,7 @@ package com.sample.edgedetection.scan
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.ExifInterface
+import androidx.exifinterface.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
